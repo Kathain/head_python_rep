d1d27692046732bf97905cd158af7cc5c01dc0a5
Katha <kateryna.krasiuk@gmail.com>
2023-06-08 14:44:37 +0200

    New Update dict_2

--- python_1_level/dict_2.py
+++ python_1_level/dict_2.py
@@ -9,8 +9,8 @@ print(d[1])   # we take a value of the first key, result -> one
 d[4] = 'four'    # if we want to add new key
 
 person = {
-    'name': 'Vasya'
-    'surname': 'Petrov'
+    'name': 'Vasya',
+    'surname': 'Petrov',
     'age': 25
 }
 print(person)
